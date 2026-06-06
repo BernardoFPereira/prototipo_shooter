41f0ad324c27c8da55099cc75095496510a01c40
BernardoFPereira <bernardofpereira@live.com>
2026-06-06 16:00:28 -0300

    Fix: Colisores da sala de caixas + Altura das frestas para arremesso

--- entities/projectile/projectile.gd
+++ entities/projectile/projectile.gd
@@ -17,7 +17,7 @@ var damage: int = 25
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	collision_area.body_entered.connect(_on_projectile_impact)
-	collision_area.area_entered.connect(_on_projectile_explosion)
+	#collision_area.area_entered.connect(_on_projectile_explosion)
 	lifetime_timer.timeout.connect(_on_lifetime_timer_timeout)
 	explosion_timer.timeout.connect(_on_explosion_timer_timeout)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -30,6 +30,7 @@ func start(dir) -> void:
 func explode():
 	explosion_timer.start()
 	speed = 0
+	direction = Vector3.ZERO
 	mesh.visible = false
 	particles.emitting = false
 	collision_area.set_deferred("monitoring", false)
@@ -62,8 +63,8 @@ func _on_projectile_impact(_body):
 	explode()
 	#call_deferred("queue_free")
 
-func _on_projectile_explosion(_area):
-	explode()
+#func _on_projectile_explosion(_area):
+	#explode()
 	#call_deferred("queue_free")
 
 func _on_explosion_timer_timeout():
