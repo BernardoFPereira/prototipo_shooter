4b6b302a163bb595a817b932955963c78d905188
BernardoFPereira <bernardofpereira@live.com>
2026-09-12 16:47:54 -0300

    Ajuste: Caixas na area de plataformas (lvl 01)

--- scripts/plataform_movement.gd
+++ scripts/plataform_movement.gd
@@ -15,26 +15,22 @@ func _ready():
 	global_position = current_node.global_position
 	target = path[current_node_idx + 1]
 
-
-func _process(delta):
+func _physics_process(delta):
 	move_to_next_target(delta * move_speed)
 
 func move_to_next_target(speed):
-	print(target)
 	current_node_idx = path.find(current_node)
-	print(current_node_idx)
 	if current_node_idx == len(path) - 1:
 		target = path[0]
 		current_node_idx = 0
 	
-	#print(current_node_idx)
-	
 	global_position = global_position.move_toward(target.global_position, speed)
+	
+	print("current pos: %s", global_position)
+	print("target_pos: %s", target.global_position)
+	
 	if global_position == target.global_position:
 		current_node = target
 		current_node_idx += 1
 		target = path[current_node_idx]
 	
-
-#func check_arrival():
-	#
--- scripts/scene_progress_area.gd
+++ scripts/scene_progress_area.gd
@@ -2,8 +2,12 @@ extends Area3D
 
 @export var level_to_load: PackedScene
 @export var is_final_level: bool
+var main_menu_scene = load("uid://d2rqkagxvdfhw")
 
 func _on_body_entered(body):
 	if body is Player:
 		body.next_level(level_to_load)
+		if is_final_level:
+			body.next_level(main_menu_scene)
+			#get_tree().change_scene_to_packed(main_menu_scene)
 		#get_tree().change_scene_to_packed(level_to_load)
