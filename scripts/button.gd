4fe17805fae030afeafdcb5a5b87487f66fd87a2
BernardoFPereira <bernardofpereira@live.com>
2026-09-14 20:40:13 -0300

    Spawner preparado para funcionar

--- scripts/button.gd
+++ scripts/button.gd
@@ -27,9 +27,9 @@ func set_state(new_state):
 	
 	match state:
 		button_states.PRESSED:
-			if linked_obj is SpawnerDoor:
-				linked_obj.spawn_enemy()
-				return
+			#if linked_obj is SpawnerDoor:
+				#linked_obj.spawn_enemy()
+				#return
 			
 			if linked_obj is Door and linked_obj.is_broken:
 				print("DOOR BROKEN BRUH")
@@ -41,9 +41,9 @@ func set_state(new_state):
 			
 			linked_obj.get_node("AnimationPlayer").play("door_open")
 			button_geo.material_override = pressed_mat
-		button_states.UNPRESSED:
-			if linked_obj is SpawnerDoor:
-				return
+		#button_states.UNPRESSED:
+			#if linked_obj is SpawnerDoor:
+				#return
 				
 			linked_obj.get_node("AnimationPlayer").play_backwards("door_open")
 			button_geo.material_override = null
